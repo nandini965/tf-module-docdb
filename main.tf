7ff0ffb61c579c4256e32f7171ebb1f133d9e1d6
nandini965 <nandhini965@gmail.com>
2023-09-14 12:15:07 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_docdb_cluster" "docdb" {
   db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.main.name
   storage_encrypted = true
   kms_key_id = var.kms_arn
-  port  var.port_no
+  port = var.port_no
 vpc_security_group_ids = [aws_security_group.main.id]
 }
 
