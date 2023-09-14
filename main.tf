bd8467b2ad80d10428f6466a72ef1521d3c52cbc
nandini965 <nandhini965@gmail.com>
2023-09-14 12:10:05 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -56,9 +56,10 @@ resource "aws_docdb_cluster_parameter_group" "main" {
   tags = "merge(var.tags, { Name = "${var.name}-${var.env}-pg" })"
 
 }
+
 resource "aws_docdb_cluster_instance" "cluster_instances" {
 count              = var.instance_count
 identifier         = "${var.name}-${var.env}-${count.index}"
 cluster_identifier = aws_docdb_cluster.main.id
-instance_class     = "var.instance_class"
+instance_class     = var.instance_class
 }
