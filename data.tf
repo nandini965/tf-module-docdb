7172a4e8798ecda5327b52ba1d06c8472f80a9ab
nandini965 <nandhini965@gmail.com>
2023-09-14 14:21:51 +0530

    filebeat

--- data.tf
+++ data.tf
@@ -1,7 +1,8 @@
-data "aws_ssm_parameter" "docdb_user" {
-  name = "${var.env}-${var.name}-docdb_user"
+
+data "aws_ssm_parameter" "db_user" {
+  name = "${var.env}.${var.name}.db_user"
 }
 
-data "aws_ssm_parameter" "docdb_pass" {
-  name = "${var.env}-${var.name}-docdb_pass"
+data "aws_ssm_parameter" "db_pass" {
+  name = "${var.env}.${var.name}.db_pass"
 }
