920ccc440499bb47d4d20f0c5f9d98d98db26fac
nandini965 <nandhini965@gmail.com>
2023-09-14 23:48:02 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -6,8 +6,7 @@ resource "aws_docdb_subnet_group" "main" {
 }
 
 
-
-resource "aws_security_group" "docdb" {
+resource "aws_security_group" "main" {
   name        = "${var.name}-${var.env}-sg"
   description = "${var.name}-${var.env}-sg"
   vpc_id      = var.vpc_id
