d759bbe30681bc6997f0db9dedb86a67856522dc
nandini965 <nandhini965@gmail.com>
2023-09-14 14:11:48 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -42,12 +42,13 @@ resource "aws_docdb_cluster" "docdb" {
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"
   skip_final_snapshot     = true
-  db_subnet_group_name = aws_docdb_subnet_group_main.name
+
+  db_subnet_group_name    = aws_docdb_subnet_group.main.name
   db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.main.name
   storage_encrypted = true
   kms_key_id = var.kms_arn
   port = var.port_no
-  vpc_security_group_ids          = [aws_security_group.main.id]
+  vpc_security_group_ids          = ["aws_security_group.main.id"]
 }
 
 resource "aws_docdb_cluster_parameter_group" "main" {
